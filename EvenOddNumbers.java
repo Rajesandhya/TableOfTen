a13e81ac84e38795cbd64959adcc54c16f1d6266
Rajesh Limbachiya <rajesandhya_143@yahoo.co.in>
2023-11-11 12:15:13 +0000

    Tabel Of Ten

---
+++ EvenOddNumbers.java
@@ -0,0 +1,14 @@
+package generatetable10;
+
+public class EvenOddNumbers
+{
+    public static void main(String[] args)
+    {
+        int n = 20; int r=20;
+        for ( int i=1; i<=n; i=i+2)
+        {
+            System.out.println(i+ " Odd Numbers ");
+
+        }
+    }
+    }
---
+++ GenerateTable10.java
@@ -0,0 +1,12 @@
+package generatetable10;
+
+public class GenerateTable10 {
+
+    public static void main(String[] args) {
+
+        for ( int a=1; a<=10; a++){
+
+            System.out.println( "10 X " + a + " = " + 10*a);
+        }
+    }
+}
