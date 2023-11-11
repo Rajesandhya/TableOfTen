b2e8b7842f1bec70a5244d6ca2c489a502ccd968
Rajesh Limbachiya <rajesandhya_143@yahoo.co.in>
2023-11-11 12:21:55 +0000

    EvenOddFileModified

--- EvenOddNumbers.java
+++ EvenOddNumbers.java
@@ -5,9 +5,9 @@ public class EvenOddNumbers
     public static void main(String[] args)
     {
         int n = 20; int r=20;
-        for ( int i=1; i<=n; i=i+2)
+        for ( int i=2; i<=n; i=i+2)
         {
-            System.out.println(i+ " Odd Numbers ");
+            System.out.println(i+ " Even Numbers ");
 
         }
     }
